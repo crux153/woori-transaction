e76ef4fbc1bbcf98da49637499b731cd85a24a5c
Crux <crux153@users.noreply.github.com>
2024-08-13 02:08:23 +0900

    Fix types

--- packages/woori-transaction-core/src/puppeteer.d.ts
+++ packages/woori-transaction-core/src/puppeteer.d.ts
@@ -1,4 +1,11 @@
+// We must add @ts-ignore to make this work even when only puppeteer-core is installed
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import type { Browser as PuppeteerBrowser } from "puppeteer";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import type {
   Browser as PuppeteerCoreBrowser,
   Page as PuppeteerCorePage,
