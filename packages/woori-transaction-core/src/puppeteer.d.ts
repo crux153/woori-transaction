f61313634079729434080dba2cd89bcd36091431
Crux <crux153@users.noreply.github.com>
2024-08-13 01:46:09 +0900

    Update dependencies

--- packages/woori-transaction-core/src/puppeteer.d.ts
+++ packages/woori-transaction-core/src/puppeteer.d.ts
@@ -1,20 +1,16 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import type puppeteer from "puppeteer";
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import type puppeteerCore from "puppeteer-core";
+import type { Browser as PuppeteerBrowser } from "puppeteer";
+import type {
+  Browser as PuppeteerCoreBrowser,
+  Page as PuppeteerCorePage,
+} from "puppeteer-core";
 
 // eslint-disable-next-line @typescript-eslint/ban-types
 type IsAny<T> = unknown extends T ? (T extends {} ? T : never) : never;
 type NotAny<T> = T extends IsAny<T> ? never : T;
 
 declare namespace Puppeteer {
-  export type Browser =
-    | NotAny<puppeteer.Browser>
-    | NotAny<puppeteerCore.Browser>;
-  export type Page = puppeteerCore.Page;
+  export type Browser = NotAny<PuppeteerBrowser> | NotAny<PuppeteerCoreBrowser>;
+  export type Page = PuppeteerCorePage;
 }
 
 export default Puppeteer;
